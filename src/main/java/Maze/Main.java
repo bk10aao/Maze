1088e659729b0555e89d3100d1d74394f5b7f0bd
Dr-Psycho-Ben <kane_500@hotmail.com>
2024-12-31 15:02:44 +0000

    adding an even larger maze

--- src/main/java/Maze/Main.java
+++ src/main/java/Maze/Main.java
@@ -8,16 +8,16 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        List<String> input = ReadFile.getInput("maze_1.txt");
-        Maze maze = new Maze(input);
+        Maze maze = new Maze(ReadFile.getInput("maze_1.txt"));
         System.out.println("BFS: " + maze.bfs());
         System.out.println("DFS: " + maze.dfs());
-        input = ReadFile.getInput("maze_2.txt");
-        maze = new Maze(input);
+        maze = new Maze(ReadFile.getInput("maze_2.txt"));
         System.out.println("BFS: " + maze.bfs());
         System.out.println("DFS: " + maze.dfs());
-        input = ReadFile.getInput("maze_3.txt");
-        maze = new Maze(input);
+        maze = new Maze(ReadFile.getInput("maze_3.txt"));
+        System.out.println("BFS: " + maze.bfs());
+        System.out.println("DFS: " + maze.dfs());
+        maze = new Maze(ReadFile.getInput("maze_4.txt"));
         System.out.println("BFS: " + maze.bfs());
         System.out.println("DFS: " + maze.dfs());
     }
